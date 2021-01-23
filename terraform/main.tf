afdf6bfde3719431854c090510ba89f25360dada
Joey Stout <joey@apollorion.com>
2021-01-23 01:07:53 -0500

    formatted terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,9 +11,9 @@ terraform {
 }
 
 locals {
-  tld = "i-dont-need-more-domains.io"
-  acm_cert = "arn:aws:acm:us-east-1:874575230586:certificate/52276146-910e-4e5c-947c-3c02a36b5444"
-  images = fileset("../idnmd/src/memes/", "*")
+  tld           = "i-dont-need-more-domains.io"
+  acm_cert      = "arn:aws:acm:us-east-1:874575230586:certificate/52276146-910e-4e5c-947c-3c02a36b5444"
+  images        = fileset("../idnmd/src/memes/", "*")
   website_files = tolist(fileset("../idnmd/build/", "**"))
 
   acm_aliases = [for i in local.images : "${lower(split(".", i)[0])}.${local.tld}"]
