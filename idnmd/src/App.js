243c12f9e405ea39570d291569cc893b120cdb02
apollorion <joey@apollorion.com>
2022-05-23 18:58:02 -0400

    fix: images

--- idnmd/src/App.js
+++ idnmd/src/App.js
@@ -6,38 +6,41 @@ const domainDetails = getDomainDetails();
 const images = process.env.ENVIRONMENT !== "test" ? importAllImages(require.context('./memes', false, /\.(png|jpe?g|svg)$/)) : {};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={getImage()} className="App-logo" alt="meme" />
-        <div>
-          {Object.keys(images).map((image, index) => {
-              return <span className={"link"} key={index}><a href={`https://${image}.${domainDetails.tld}`}>{image}</a> {index === Object.keys(images).length -1 ? "" : " | "} </span>
-          })}
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={getImage()} className="App-logo" alt="meme" />
+                <div>
+                    {Object.keys(images).map((image, index) => {
+                        return <span className={"link"} key={index}><a href={`https://${image}.${domainDetails.tld}`}>{image}</a> {index === Object.keys(images).length -1 ? "" : " | "} </span>
+                    })}
+                </div>
+                <br/>
+                <a href={"https://github.com/Apollorion/i-dont-need-more-domains"}><img src={github} alt={"github"} /></a>
+            </header>
         </div>
-          <br/>
-          <a href={"https://github.com/Apollorion/i-dont-need-more-domains"}><img src={github} alt={"github"} /></a>
-      </header>
-    </div>
-  );
+    );
 }
 
 function getDomainDetails(){
     const domain = window.location.host.split(".");
 
     let subdomain = "root";
+    let tld = domain[0];
     if(domain.length > 2 && domain[0] !== "www" && domain[0] !== "stage"){
         subdomain = domain[0];
+        tld = domain[1]
     }
 
     return {
-        "subdomain": subdomain,
-        "tld": process.env.REACT_APP_TLD ? process.env.REACT_APP_TLD : "i-dont-need-more-domains-dev.io:3000"
+        subdomain,
+        tld
     };
 }
 
 // Determines which image to show based off subdomain.
 function getImage(){
+    console.log("DOMAIN_DETAILS", domainDetails);
     if(domainDetails.subdomain !== "root" && domainDetails.subdomain !== "www"){
         // Show current subdomains image
         return images[domainDetails.subdomain];
@@ -51,6 +54,9 @@ function getImage(){
 function importAllImages(r) {
     let keys = r.keys().map(x => x.split(".")[1].substring(1));
     let values = r.keys().map(r);
+
+    console.log("KEYS", keys);
+    console.log("VALUES", values)
 
     let rv = {};
     for(let i in keys){
